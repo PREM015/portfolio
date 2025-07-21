3de16c128de42076575c9955bd1df6ed0f2ba5d8
PREM015 <152625984+PREM015@users.noreply.github.com>
2025-07-21 15:45:31 +0530

    small fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@
 import "./globals.css";
 import { Metadata } from "next";
 import Navbar from "@/app/components/common/Navbar";
-
+import Footer from "@/app/components/common/Footer";
 export const metadata: Metadata = {
   title: "Prem raj",
   description: "Prem Raj's Portfolio",
@@ -27,6 +27,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         {/* ✅ Main content area */}
         <main className="flex-1 pt-[90px] w-full z-10 relative">
           {children}
+
+
+          <Footer/>
         </main>
       </body>
     </html>
